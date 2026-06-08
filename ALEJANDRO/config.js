1cb46db9b2a4ad1b2ee951d7f8160c139fca97f4
licaptio <licaptio42@gmail.com>
2026-06-08 07:54:35 -0600

    Update config.js

--- ALEJANDRO/config.js
+++ ALEJANDRO/config.js
@@ -18,7 +18,7 @@ export const firebaseConfig = {
 
 // 🟢 Supabase
 export const supabaseUrl = "https://cvpbtjlupswbyxenugpz.supabase.co";
-export const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN2cGJ0amx1cHN3Ynl4ZW51Z3B6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc3MDIxOTQsImV4cCI6MjA2MzI3ODE5NH0.iiJsYM3TtaGPdeCtPcEXwAz3LfFc1uJGECEvOErvrqY";
+export const supabaseAnonKey = "sb_publishable_SQ7Q5LFJqlxVzwNTxcIyzQ_8F1bqyiX";
 
 // ✅ Inicializar Firebase
 const app = initializeApp(firebaseConfig);
