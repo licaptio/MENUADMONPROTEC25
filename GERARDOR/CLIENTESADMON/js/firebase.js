f2b68f561d6c6a81f66797d0199d2c6c68fbf54a
licaptio <licaptio42@gmail.com>
2026-06-21 09:28:44 -0600

    Delete GERARDOR/CLIENTESADMON directory

--- GERARDOR/CLIENTESADMON/js/firebase.js
+++
@@ -1,6 +0,0 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
-import { firebaseConfig } from "../configuraciones/firebaseConfig.js";
-
-export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
